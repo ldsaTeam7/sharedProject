38ebc77fee55aa360d1ef8eb30999ccb816066e2
polajev <alekseipolajev@hotmail.com>
2017-06-02 10:47:24 +0200

    the filter script now has comments

--- filter.py
+++ filter.py
@@ -3,9 +3,16 @@
 import sys
 import pysam
 
+#The pysam module is the module that can process CRAM files
+
 cramFile = pysam.AlignmentFile("HG00096.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram", "rc")
+
+#This code opens up the cram file for reading. "rc" means "read" and "cram format". 
+
 for line in cramFile.fetch():
         list = str(line).split()
+		#every line in the cram file will be split by column. 
         print(list[9])
+		# We need the 9th column. This is where the DNA sequence is. 
 cramFile.close()
 print("Done!")
